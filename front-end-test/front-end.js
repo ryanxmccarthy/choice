a4e26748d4940d0882f441b06c5554c36e4238c3
Jane Aguero <jane.m.aguero@gmail.com>
2018-02-07 20:49:57 -0500

    front end testing folder

---
+++ front-end-test/front-end.js
@@ -0,0 +1,12 @@
+
+// Animation Script
+
+var expand = false;
+function toggle() {
+  expand = !expand;
+
+  var toggleDiv = document.getElementById('toggleDiv');
+  toggleDiv.classList.toggle('expand');
+
+};
+ 
