20a9e50e721030fc4950528e5bc36bee9ce9f157
Benjamin Lawson <lawbe101@yahoo.com>
2018-02-09 07:38:38 -0500

    database collections for concerts/restaurants

--- models/Concert.js
+++ models/Concert.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const articleSchema = new Schema({
-  title: { 
+const consertSchema = new Schema({
+  userID: {
+    type: String,
+    required: true
+  },
+  artist: { 
     type: String, 
     required: true 
+  },
+  location: {
+    type: String,
+    required: true
   },
   date: { 
     type: Date, 
@@ -17,7 +25,7 @@ const articleSchema = new Schema({
   },
 });
 
-const Article = mongoose.model("Article", articleSchema);
+const Article = mongoose.model("Article", consertSchema);
 
 module.exports = Article;
 
---
+++ models/Restaurant.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const restaurantSchema = new Schema({
+  userID: {
+    type: String,
+    required: true
+  },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  location: { 
+    type: String,
+    required: true 
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+});
+
+const Article = mongoose.model("Article", restaurantSchema);
+
+module.exports = Article;
+
