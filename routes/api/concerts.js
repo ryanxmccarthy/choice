39a9720cf44f3da9d1d161bbdcdc5f21ac7a513c
Ryan McCarthy <ryanxmccarthy@gmail.com>
2018-02-09 11:31:18 -0500

    almost fixed concerts api ?????

--- routes/api/concerts.js
+++ routes/api/concerts.js
@@ -1,16 +1,15 @@
-const router = require("express").Router();
+const concertRouter = require("express").Router();
 const concertController = require("../../controllers/concertController");
 
 // Matches with "/api/concerts"
-// router.route("/")
-//   .get(concertController.findAll)
-//   .post(concertController.create);
+concertRouter.route("/concerts")
+  .get(concertController.findAll)
+  .post(concertController.create);
 
 // Matches with "/api/concerts/:id"
-// router
-//   .route("/:id")
+// concertRouter.route("/:id")
 //   .get(concertController.findById)
 //   .put(concertController.update)
 //   .delete(concertController.remove);
 
-module.exports = router;
+module.exports = concertRouter;
---
+++ routes/api/restaurants.js
@@ -0,0 +1,15 @@
+const restaurantRouter = require("express").Router();
+const restaurantController = require("../../controllers/restaurantController");
+
+// Matches with "/api/restaurants"
+restaurantRouter.route("/restaurants")
+  .get(restaurantController.findAll)
+  .post(restaurantController.create);
+
+// Matches with "/api/restaurants/:id"
+restaurantRouter.route("/:id")
+  .get(restaurantController.findbyId)
+  .put(restaurantController.update)
+  .delete(restaurantController.remove);
+
+module.exports = restaurantRouter;
