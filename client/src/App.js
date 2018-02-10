5ffa3e74c0ade4d0aa4d5910615ea5a0e5f07bf7
Jane Aguero <jane.m.aguero@gmail.com>
2018-02-10 11:02:17 -0500

    updates to components and App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Choice from "./pages/Choice";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import Dashboard from './components/Dashboard'
 
-const App = () =>
-  <Router>
-    <div>
-      <Nav />
-      <Dashboard />
-      <Switch>
-        <Route exact path="/" component={Choice} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>;
+class App extends Component {
+  render() {
+    return(
+      <Router>
+        <div>
+          <Nav />
+          <Dashboard />
+          <Switch>
+            <Route exact path="/" component={Choice} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,41 +1,40 @@
 import React from "react";
 import "./dashboard.css";
 
-var expand = false;
-function toggle() {
-  expand = !expand;
+const expand = false;
 
-  var toggleDiv = document.getElementById('toggleDiv');
-  toggleDiv.classList.toggle('expand');
+export const Dashboard = () => {
 
+  const toggle = () => {
+    let expand = !expand;
+    document.getElementById('toggleDiv').classList.toggle('expand');
+  };
+
+  return(
+  	<div className="container-fluid" id="toggleDiv">
+      <div className="row">
+        <div id="bands">
+          <a href="#"><div className="grid-item"></div></a>
+          <a href="#"><div className="grid-item"></div></a>
+          <a href="#"><div className="grid-item"></div></a>
+          <a href="#"><div className="grid-item"></div></a>
+          <a href="#"><div className="grid-item"></div></a>
+          <a href="#"><div className="grid-item"></div></a>
+        </div>
+
+        <div id="maps">
+          <button className="toggle" onClick={this.toggle()}>Expand</button>
+        </div>
+      </div>
+      
+      <div className="row" id="eats">
+        <a href="#"><div className="grid-item"></div></a>
+        <a href="#"><div className="grid-item"></div></a>
+        <a href="#"><div className="grid-item"></div></a>
+        <a href="#"><div className="grid-item"></div></a>
+        <a href="#"><div className="grid-item"></div></a>
+        <a href="#"><div className="grid-item"></div></a>
+      </div>
+    </div>
+  );
 };
-
-const Dashboard = () => (
-	<div className="container-fluid" id="toggleDiv">
-    <div className="row">
-      <div id="bands">
-        <a href="#"><div className="grid-item"></div></a>
-        <a href="#"><div className="grid-item"></div></a>
-        <a href="#"><div className="grid-item"></div></a>
-        <a href="#"><div className="grid-item"></div></a>
-        <a href="#"><div className="grid-item"></div></a>
-        <a href="#"><div className="grid-item"></div></a>
-      </div>
-
-      <div id="maps">
-        <button className="toggle" onclick="toggle()">Expand</button>
-      </div>
-    </div>
-    
-    <div className="row" id="eats">
-      <a href="#"><div className="grid-item"></div></a>
-      <a href="#"><div className="grid-item"></div></a>
-      <a href="#"><div className="grid-item"></div></a>
-      <a href="#"><div className="grid-item"></div></a>
-      <a href="#"><div className="grid-item"></div></a>
-      <a href="#"><div className="grid-item"></div></a>
-    </div>
-  </div>;
-);
-
-export default Dashboard;
