d6f8afc57a77da49143c58777377cc28360bcdbe
Ryan McCarthy <ryanxmccarthy@gmail.com>
2018-02-12 23:31:14 -0500

    added props to nav and dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,110 +2,130 @@ import React from "react";
 import "./dashboard.css";
 import { Card, CardTitle, CardText, CardImg, CardImgOverlay } from 'reactstrap';
 
-export default class Dashboard extends React.Component{
+export default class Dashboard extends React.Component {
+  state = {
+    search: '',
+    events: [],
+  }
 
   render() {
     return(
-    	<div className="container-fluid" id="toggleDiv">
-        <div className="row">
-          <div id="bands">
-            <a href="/bands/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-            <a href="/bands/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-            <a href="/bands/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-            <a href="/bands/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-            <a href="/bands/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-            <a href="/bands/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-          </div>
+      <div>
+        <div id='results'>
+          { this.props.state.events.map(event => (
+              <a href="/bands" key={event.id}>
+                <Card inverse className="grid-item" >
+                  <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                  <CardImgOverlay className="imgOverlay">
+                    <CardTitle>{event.id}</CardTitle>
+                    <CardText>{event.venue.city}</CardText>
+                  </CardImgOverlay>
+                </Card>
+              </a>
+            ))}
         </div>
 
-        <div className="row">
-          <div className='map'>
-            <img alt="" src="https://www.wired.com/wp-content/uploads/2016/11/GoogleMap-1-1200x630-e1479510841629.jpg" />
+        {/*<div className="container-fluid" id="toggleDiv">
+          <div className="row">
+            <div id="bands">
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+            </div>
+          </div>*/}
+
+          {/*<div className="row">
+            <div id="maps">
+              <img src="https://developers.google.com/maps/documentation/urls/images/map-no-params.png" alt=" " />
+            </div>
+          </div>*/}
+          
+          {/*<div className="row">
+            <div id="eats">
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+              <a href="/eats"><Card inverse className="grid-item">
+                <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
+                <CardImgOverlay className="imgOverlay">
+                  <CardTitle>Name</CardTitle>
+                  <CardText>Content</CardText>
+                </CardImgOverlay>
+              </Card></a>
+            </div>
           </div>
-        </div>
-        
-        <div className="row">
-          <div id="eats">
-            <a href="/eats/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-            <a href="/eats/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-            <a href="/eats/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-            <a href="/eats/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-            <a href="/eats/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-            <a href="/eats/details"><Card inverse className="grid-item">
-              <CardImg src="https://threecheers.co.uk/assets/Latchmere-battersea-food-4-318x270.jpg" alt="Card image cap" />
-              <CardImgOverlay className="imgOverlay">
-                <CardTitle>Name</CardTitle>
-                <CardText>Content</CardText>
-              </CardImgOverlay>
-            </Card></a>
-          </div>
-        </div>
+        </div>*/}
       </div>
     );
   }
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -19,114 +19,81 @@ import {
   DropdownItem } from 'reactstrap';
 
 const defaultState = {
-  search: ''
+  search: '',
+  events: [],
 }
 
 export default class Navi extends React.Component {
 
   state = defaultState;
 
-  handleInputChange = (event) => {
-    const { name, value }= event.target;
-    this.setState({ [name]: value });
-    console.log("search input change");
-  };
-
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    console.log(`search: ${this.state.search}`);
-    this.setState(defaultState);
-  };
-
   navSignIn = () => {
+    document.getElementById('modalBox').style.display = "block";
     document.getElementById('modal').style.display = "block";
-    document.getElementById('modal-dialog').style.display = "block";
     console.log("Nav sign in");
   };
 
   constructor(props) {
     super(props);
-
     this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpen: false
+      isOpen: true
     };
   }
+
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
 
-  state = { events: [] };
-
-  componentDidMount = () => {
-    fetch('https://rest.bandsintown.com/artists/watain/events?app_id=lost&ffound')
-    .then(results => {
-      return results.json();
-      console.log(results.json());
-    }).then(data => {
-      console.log(data)
-    //   let events = data.results.map(event => {
-    //     console.log(event.json());
-    //   })
-
-    //   this.setState({events: events});
-    //   console.log('state', this.state.events);
-    })
-  };
-
   render() {
     return (
       <div>
-        {/*<div className='container2'>
-          <div className='container1'>
-            {this.state.pictures}
-          </div>
-        </div>*/}
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/"><h1>Choice</h1></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <UncontrolledDropdown className="dropdown">
+                  <DropdownToggle nav caret>
+                    Dashboard
+                  </DropdownToggle>
+                  <DropdownMenu >
+                    <DropdownItem href="/bands">
+                      Your Bands
+                    </DropdownItem>
+                    <DropdownItem href="/eats">
+                      Your Eats
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </NavItem>
+              <NavItem>
+                <Form inline className="search">
+                  <FormGroup>
+                    <Input type="search" 
+                            name="search" 
+                            id="searchBar" 
+                            placeholder="Search"
+                            onChange={this.props.input} />
+                  </FormGroup>
+                  <Button color="success"
+                          id="searchBtn"
+                          className="search"
+                          onClick={this.props.form}>Search</Button>
+                  <Button color="link" 
+                          onClick={this.navSignIn}>Sign In</Button>
+                </Form>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
 
-        <div>
-          <Navbar color="faded" light expand="md">
-            <NavbarBrand href="/"><h1>Choice</h1></NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <UncontrolledDropdown className="dropdown" nav inNavbar>
-                    <DropdownToggle nav caret>
-                      Dashboard
-                    </DropdownToggle>
-                    <DropdownMenu >
-                      <DropdownItem href="/bands">
-                        Your Bands
-                      </DropdownItem>
-                      <DropdownItem href="/eats">
-                        Your Eats
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                </NavItem>
-                <NavItem>
-                  <Form inline className="search">
-                    <FormGroup>
-                      <Input type="search" 
-                              name="search" 
-                              id="searchBar" 
-                              placeholder="Search"
-                              onChange={this.handleInputChange} />
-                    </FormGroup>
-                    <Button color="success"
-                            id="searchBtn"
-                            className="search"
-                            onClick={this.handleFormSubmit}>Search</Button>
-                    <Button color="link" 
-                            onClick={this.navSignIn}>Sign In</Button>
-                  </Form>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </Navbar>
+        <div id='results'>
         </div>
+
       </div>
     );
   }
