81bb135e835b2d95ac1de1ad1e77776219385540
Jane Aguero <jane.m.aguero@gmail.com>
2018-02-13 18:45:57 -0500

    deleted detail component, updated dashboard, gave up on nav formatting

--- client/src/pages/Details/Details.js
+++ client/src/pages/Details/Details.js
@@ -3,8 +3,8 @@ import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import Navi from "../../components/Nav";
 import Modal from "../../components/Modal";
-import Detail from "../../components/Detail";
 import Footer from "../../components/Footer";
+import { Media } from 'reactstrap';
 
 export default class Details extends React.Component {
   render() {
@@ -12,7 +12,23 @@ export default class Details extends React.Component {
       <section>
         <Navi />
         <Modal />
-        <Detail />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <Media>
+                <Media left href="#">
+                  <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
+                </Media>
+                <Media body>
+                  <Media heading>
+                    Media heading
+                  </Media>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </Media>
+              </Media>
+            </div>
+          </div>
+        </div>
         <Footer />
       </section>
     );
